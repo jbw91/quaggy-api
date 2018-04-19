f0bfbc677c88b47317d237ccb971d0f6c1e6ddd6
John Woodruff <johnwoodruff91@gmail.com>
2018-04-19 14:12:56 -0600

    Add CRUD methods for channel service

--- src/channel/channel.controller.ts
+++ src/channel/channel.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { Channel } from './channel.entity';
 import { ChannelService } from './channel.service';
 
@@ -7,7 +7,27 @@ export class ChannelController {
   constructor(private readonly channelService: ChannelService) {}
 
   @Get()
-  findAll(): Promise<Channel[]> {
-    return this.channelService.findAll();
+  async findAll(): Promise<Channel[]> {
+    return await this.channelService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param() params): Promise<Channel> {
+    return await this.channelService.findOne(params.id);
+  }
+
+  @Post()
+  async createChannel(@Body() channel: Channel): Promise<Channel> {
+    return await this.channelService.createChannel(channel);
+  }
+
+  @Put(':id')
+  async updateChannel(@Param() params, @Body() channel: Channel): Promise<void> {
+    await this.channelService.updateChannel(params.id, channel);
+  }
+
+  @Delete(':id')
+  async deleteChannel(@Param() params): Promise<void> {
+    await this.channelService.deleteChannel(params.id);
   }
 }
--- src/channel/channel.service.ts
+++ src/channel/channel.service.ts
@@ -12,4 +12,24 @@ export class ChannelService {
   async findAll(): Promise<Channel[]> {
     return await this.channelRepository.find();
   }
+
+  async findOne(id: number): Promise<Channel> {
+    return await this.channelRepository.findOneById(id);
+  }
+
+  async createChannel(channel: Channel): Promise<Channel> {
+    const newChannel = new Channel();
+    newChannel.name = channel.name;
+    newChannel.public = channel.public;
+    newChannel.team = channel.team;
+    return await this.channelRepository.save(newChannel);
+  }
+
+  async updateChannel(id: number, channel: Channel): Promise<void> {
+    await this.channelRepository.updateById(id, channel);
+  }
+
+  async deleteChannel(id: number): Promise<void> {
+    await this.channelRepository.deleteById(id);
+  }
 }
